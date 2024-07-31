22e074c4f3c64d6f85e2b04c47ea11e590b61c0a
NDS3K <160611407+NDS3K@users.noreply.github.com>
2024-07-31 17:37:55 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
-#include <Arduino.h>
+#include <Configs.h>
+#include <Controller.h>
+#include <ColorSensor.h>
+#include <DistanceSensor.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int  ball_val;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup() 
+{
+  initialize();
+  Sensor_ini();
+  distance_ini();
+  setupPS2controller();
+  
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop() 
+{
+  tcs.getRawData(&r, &g, &b, &c);
+  ball_val = rgb_to_hsv(r, g, b);
+  classify(ball_val);
+  controller();
+  
+  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+  delay(50);
 }
